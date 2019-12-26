582a805a6296db5c8b0915a0dbdb98443552922b
kerolos hanna saad <merna.hanna92@gmail.com>
2019-12-26 23:23:17 +0200

    audio

--- src/components/list/list_audio.js
+++ src/components/list/list_audio.js
@@ -3,6 +3,8 @@ import './list.css'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faTimes} from '@fortawesome/free-solid-svg-icons'
 import AddList from './add_list'
+import { Button, Modal} from 'antd';
+
 
 export class ListAudio extends Component {
 
@@ -11,46 +13,58 @@ export class ListAudio extends Component {
     this.state = {
       track: [],
       query: "",
-      filteredData: []
+      filteredData: [],
+      visible: false,
+      fields: {},
+      errors: {},
+      srcAudio:''
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.submitnewTrack = this.submitnewTrack.bind(this);
+     this.playAudio = this.playAudio.bind(this);
+     this.renderList = this.renderList.bind(this)
   }
 
   componentDidMount() {
-    fetch("https://api.myjson.com/bins/hw8lz")
-      .then(res => res.json())
-      .then(
-        (res) => {
-          const {
-            query
-          } = this.state;
-          const filteredData = res.tracks.filter(element => {
-            return element.name.toLowerCase().includes(query.toLowerCase());
-          });
-
-          this.setState({
-            track: res.tracks,
-            filteredData
-          });
-        },
-        (err) => {
-          console.log(err)
-        }
-      )
+    this.getTracks()
   }
 
-  handleInputChange = event => {
-    const query = event.target.value;
+  getTracks(){
+   fetch("https://api.myjson.com/bins/hw8lz")
+     .then(res => res.json())
+     .then(
+       (res) => {
+         const {
+           query
+         } = this.state;
 
-    this.setState(prevState => {
-      const filteredData = prevState.track.filter(element => {
-        return element.name.toLowerCase().includes(query.toLowerCase());
-      });
+         const filteredData = res.tracks.filter(element => {
+           return element.name.toLowerCase().includes(query.toLowerCase());
+         });
+         this.setState({
+           track: res.tracks,
+           filteredData
+         });
 
-      return {
-        query,
-        filteredData
-      };
-    });
+       },
+       (err) => {
+         console.log(err)
+       }
+     )
+  }
+
+  search = event => {
+       const query = event.target.value;
+       this.setState(prevState => {
+         const filteredData = prevState.track.filter(element => {
+           return element.name.toLowerCase().includes(query.toLowerCase());
+         });
+
+         return {
+           query,
+           filteredData
+         };
+       })
   };
 
   renderList() {
@@ -58,22 +72,34 @@ export class ListAudio extends Component {
       track,
       filteredData
     } = this.state
-    return (filteredData == [] ? track : filteredData).map((d, index) => {
-      return <div className = "listaduio" > 
+    return (track ).map((d, index) => {
+      return <div className = "listaduio"
+      onClick = {
+        this.playAudio.bind(this,d.url)
+      } >
                 < li > {`${d.name} - ${d.artist}`}
                   < div className = "inline" >
-                    <h5 > {d.length} < /h5>  
-                    < FontAwesomeIcon icon = {
+                    <h5 > {d.length} </h5>  
+                    < a onClick = {
+                        this.deleteElement.bind(this, index)
+                      } > < FontAwesomeIcon icon = {
                       faTimes
                     }
-                    onClick = {
-                      this.deleteElement.bind(this, index)
-                    }
                     />  
+                    < /a>
                   </div>  
                 </li>
               </div>
     })
+  }
+
+  playAudio(url){
+   
+           this.setState({
+             srcAudio: url
+           });
+           var player = document.getElementById('audio');
+           player.play()
   }
 
   deleteElement(index) {
@@ -87,16 +113,122 @@ export class ListAudio extends Component {
     //         return i !== index;
     //       })
     //     })
-    this.state.filteredData == [] ? this.setState({
+    console.log(index)
+    this.setState({
           track :this.state.track.filter(function (e, i) {
               return i !== index;
             }) })
-            : this.setState({
-                 track: this.state.filteredData.filter(function (e, i) {
-              return i !== index;
-            })})
-        console.log(this.state.track)
+         
   }
+
+  handleChange(e) {
+    let fields = this.state.fields;
+    fields[e.target.name] = e.target.value;
+    this.setState({
+      fields
+    });
+
+  }
+
+  submitnewTrack(e) {
+    e.preventDefault();
+    if (this.validateForm()) {
+      let fields = {};
+      fields["name"] = "";
+      fields["length"] = "";
+      fields["artist"] = "";
+      fields["url"] = "";
+      this.setState({
+        fields: fields
+      });
+     this.setState({
+       track: this.state.track.concat({
+         name: this.state.fields.name,
+         length: parseFloat(this.state.fields.length),
+         artist: this.state.fields.artist,
+         url: this.state.fields.url
+       }),
+       visible: false,
+     });
+     }
+     }
+
+  validateForm() {
+    let fields = this.state.fields;
+    let errors = {};
+    let formIsValid = true;
+
+    if (!fields["name"]) {
+      formIsValid = false;
+      errors["name"] = "*Please enter your name.";
+    }
+
+    if (typeof fields["name"] !== "undefined") {
+      if (!fields["name"].match(/^[a-zA-Z ]*$/)) {
+        formIsValid = false;
+        errors["name"] = "*Please enter alphabet characters only.";
+      }
+    }
+
+    if (!fields["url"]) {
+      formIsValid = false;
+      errors["url"] = "*Please enter your path.";
+    }
+
+    if (typeof fields["url"] !== "undefined") {
+      //regular expression for url validation
+      var pattern = new RegExp(/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)\.(mp3)*\/?$/);
+      if (!pattern.test(fields["url"])) {
+        formIsValid = false;
+        errors["url"] = "*Please enter valid Url path.";
+      }
+    }
+
+    if (!fields["artist"]) {
+      formIsValid = false;
+      errors["artist"] = "*Please enter artist name.";
+    }
+
+    if (typeof fields["artist"] !== "undefined") {
+      if (!fields["artist"].match(/^[a-zA-Z ]*$/)) {
+        formIsValid = false;
+        errors["artist"] = "*Please enter alphabet characters only.";
+      }
+    }
+
+    if (!fields["length"]) {
+      formIsValid = false;
+      errors["length"] = "*Please enter your length.";
+    }
+
+    if (typeof fields["length"] !== "undefined") {
+      if (!fields["length"].match(/^[0-9\b]\.[0-9\b]+$/)) {
+        formIsValid = false;
+        errors["length"] = "*Please enter length Like 1.1";
+      }
+    }
+
+    this.setState({
+      errors: errors
+    });
+    return formIsValid;
+  }
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  handleOk = e => {
+    this.submitnewTrack(e);
+  };
+
+  handleCancel = e => {
+    this.setState({
+      visible: false,
+    });
+  };
 
   render() {
     return ( <div className = "container" >
@@ -106,15 +238,68 @@ export class ListAudio extends Component {
           this.state.query
         }
         onChange = {
-          this.handleInputChange
+          this.search
         }
         style={{color:'black'}}
      /> 
      </form >
-      <audio id = "audio1"
-      preload controls > Your browser does not support HTML5 Audio!😢 < /audio> 
+      < audio controls className = "player"
+      preload = "false" id="audio">
+        < source id = "audiosource"
+        src = {
+          this.state.srcAudio
+        }
+        />
+        </audio> 
       <ul > {this.renderList()} </ul> 
-      < AddList/>
+      < Button type = "primary"
+      style = {
+        {
+          width: "20%",
+          fontSize: "1rem"
+        }
+      }
+      onClick = {
+          this.showModal
+        } > ADD NEW TRACK </Button> 
+        <Modal
+          title = "ADD TRACK"
+          visible = {this.state.visible}
+          onOk = {this.handleOk}
+          onCancel = {this.handleCancel} >
+        <form method = "post"
+          name = "newTrack"
+          onSubmit = {this.submitnewTrack} >
+        <label > Track Name </label>  
+        <input type = "text"
+          name = "name"
+          value = {this.state.fields.name}
+          onChange = {this.handleChange}
+        />  
+        <div className = "errorMsg" > {this.state.errors.name} </div>  
+        <label > Track Path </label>  
+        <input type = "text"
+          name = "url"
+          value = {this.state.fields.url}
+          onChange = {this.handleChange}
+        />  
+        <div className = "errorMsg" > {this.state.errors.url} </div>  
+        <label > Artist Name </label>  
+        <input type = "text"
+          name = "artist"
+          value = {this.state.fields.artist}
+          onChange = {this.handleChange}
+        />  
+        <div className = "errorMsg" > {this.state.errors.artist} </div>  
+        <label > Track Length </label>  
+        <input type = "length"
+          name = "length"
+          value = {this.state.fields.length}
+          onChange = {this.handleChange}
+        />  
+        <div className = "errorMsg" > {this.state.errors.length} </div>  
+         </form> 
+         </Modal>
       </div>
     )
   }
